1a38b69598bcc7e4077178f70fb580dfd8ac2329
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2023-03-18 17:59:53 +0530

    main.dart

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              child: Text('click Me'),
+              onPressed: () {
+
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
