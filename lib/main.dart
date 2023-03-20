35dd8c6c7b8620634fffc028da4371998590f014
Sagar Kumar <107524446+Sagar123Kumar@users.noreply.github.com>
2023-03-20 08:17:41 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(const XyloPhone());
+}
 
-class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({Key? key}) : super(key: key);
+class XyloPhone extends StatefulWidget {
+  const XyloPhone({Key? key}) : super(key: key);
+
+  @override
+  State<XyloPhone> createState() => _XyloPhoneState();
+}
+
+class _XyloPhoneState extends State<XyloPhone> {
+  Expanded repeatUi({Color? color, int? soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color, // Text Color
+        ),
+        onPressed: () => setState(() {
+          final player = AudioPlayer();
+
+          player.play(AssetSource('assets_note$soundNumber.wav'));
+        }),
+        child: const Text(''),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: TextButton(
-              child: Text('click Me'),
-              onPressed: () {
-
-              },
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text('Play with me'),
+          backgroundColor: Colors.black,
         ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          repeatUi(color: Colors.red, soundNumber: 1),
+          repeatUi(color: Colors.yellow, soundNumber: 2),
+          repeatUi(color: Colors.white, soundNumber: 3),
+          repeatUi(color: Colors.green, soundNumber: 4),
+          repeatUi(color: Colors.orange, soundNumber: 5),
+          repeatUi(color: Colors.pink, soundNumber: 6),
+        ]),
       ),
     );
   }
